dc72ddca6f8e462448fece6b11e1dabbdfb75e33
Lazar <lazar.stanojevic@pmf.edu.rs>
2018-06-23 00:23:07 +0200

    Dodati prozori

--- FloodIt/MainWindow.xaml.cs
+++ FloodIt/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using FloodIt.View;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +20,28 @@ namespace FloodIt
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private Page window;
+        public Page Window {
+            get => window;
+            set {
+                window = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Window"));
+            }
+        }
+
         public MainWindow()
         {
+
             InitializeComponent();
+            DataContext = this;
+
+            Window = new MainMenu(this);
+
         }
     }
 }
